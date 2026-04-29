6ce600ebd9cf9946a427b0637548ab3e3b947d9d
Raoul Nuijten <r.c.y.nuijten@tue.nl>
2026-04-29 16:40:40 +0200

    🐛 Persist roles correctly

--- src/routes/(app)/(auth)/auth/+page.server.ts
+++ src/routes/(app)/(auth)/auth/+page.server.ts
@@ -64,7 +64,7 @@ export const actions = {
           password: `{bcrypt}${hash}`,
           firstName,
           lastName,
-          roles: JSON.stringify(roles),
+          roles,
         })
         .executeTakeFirstOrThrow();
     } catch (e) {
